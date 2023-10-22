ba9511878068fa39c19a1e27f087872ae7fa678b
Gyutae Lee <gyutae07@gmail.com>
2023-10-23 00:54:33 +0900

    style: apply code convention

--- Script/Runtime/MonoBehaviourSingleton.cs
+++ Script/Runtime/MonoBehaviourSingleton.cs
@@ -4,7 +4,8 @@ namespace qbot.Utility
 {
     public class MonoBehaviourSingleton<T> : MonoBehaviour where T : Component
     {
-        private static T instance;
+#region Properties
+
         public static T Instance
         {
             get
@@ -13,20 +14,39 @@ namespace qbot.Utility
                 {
                     return instance;
                 }
-                
+
                 instance = FindObjectOfType<T>();
                 if (instance != null)
                 {
                     return instance;
                 }
-                
+
                 var obj = new GameObject(typeof(T).Name);
                 instance = obj.AddComponent<T>();
 
                 return instance;
-
             }
             set => instance = value;
         }
+
+#endregion
+
+#region Fields
+
+        private static T instance;
+
+#endregion
+
+#region Monobehaviour functions
+
+        protected virtual void Awake()
+        {
+            if (instance != null)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+#endregion
     }
 }
