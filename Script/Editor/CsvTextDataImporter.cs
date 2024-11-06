cc7076bf5038ae4e455aa3ca8d8889afc2d40300
Gyutae Lee <gyutae07@gmail.com>
2024-11-06 09:28:44 +0700

    fix: modify CsvTextDataImporter

--- Script/Editor/CsvTextDataImporter.cs
+++ Script/Editor/CsvTextDataImporter.cs
@@ -6,30 +6,49 @@ using UnityEngine;
 
 namespace qbot.Utility
 {
-    public class TsvTextDataImporter : EditorWindow
+    public class CsvTextDataImporter : EditorWindow
     {
-        private string _tsvFilePath;
+        private static readonly string ProjectPrefix = Application.dataPath.GetHashCode().ToString();
+
+        private string _csvFilePath;
+        private string CsvFilePath
+        {
+            get => EditorPrefs.GetString($"{ProjectPrefix}.{nameof(CsvTextDataImporter)}.{nameof(CsvFilePath)}");
+            set => EditorPrefs.SetString($"{ProjectPrefix}.{nameof(CsvTextDataImporter)}.{nameof(CsvFilePath)}", value);
+        }
+
         private TextDataContainer _koreanDataContainer;
         private TextDataContainer _englishDataContainer;
 
-        [MenuItem("Tools/TSV Text Data Importer")]
+        [MenuItem("qbot/Utility/CSV Text Data Importer")]
         public static void ShowWindow()
         {
-            var window = GetWindow<TsvTextDataImporter>("TSV Text Data Importer");
+            var window = GetWindow<CsvTextDataImporter>("CSV Text Data Importer");
             window.minSize = new Vector2(400, 200);
             window.maxSize = new Vector2(400, 200);
         }
 
+        private void OnEnable()
+        {
+            _csvFilePath = CsvFilePath;
+        }
+
         private void OnGUI()
         {
-            GUILayout.Label("Select TSV file and assign containers for each language", EditorStyles.boldLabel);
+            GUILayout.Label("Select CSV file and assign containers for each language", EditorStyles.boldLabel);
 
-            // TSV file path and button arranged in a single line
+            // CSV file path and button arranged in a single line
             EditorGUILayout.BeginHorizontal();
-            _tsvFilePath = EditorGUILayout.TextField("TSV File Path", _tsvFilePath);
-            if (GUILayout.Button("Find TSV File", GUILayout.Width(100)))
+            var newCsvFilePath = EditorGUILayout.TextField("CSV File Path", _csvFilePath);
+            if (newCsvFilePath != _csvFilePath)
             {
-                _tsvFilePath = EditorUtility.OpenFilePanel("Select TSV File", "", "tsv");
+                _csvFilePath = newCsvFilePath;
+                CsvFilePath = newCsvFilePath;
+            }
+
+            if (GUILayout.Button("Find CSV File", GUILayout.Width(100)))
+            {
+                CsvFilePath = EditorUtility.OpenFilePanel("Select CSV File", "", "csv");
             }
 
             EditorGUILayout.EndHorizontal();
@@ -39,7 +58,7 @@ namespace qbot.Utility
             _englishDataContainer = (TextDataContainer)EditorGUILayout.ObjectField("English Data Container", _englishDataContainer, typeof(TextDataContainer), false);
 
             // Enable Import button only when all fields are properly set
-            GUI.enabled = !string.IsNullOrEmpty(_tsvFilePath) && _koreanDataContainer != null && _englishDataContainer != null;
+            GUI.enabled = !string.IsNullOrEmpty(CsvFilePath) && _koreanDataContainer != null && _englishDataContainer != null;
             if (GUILayout.Button("Import Data"))
             {
                 ImportData();
@@ -50,9 +69,9 @@ namespace qbot.Utility
 
         private void ImportData()
         {
-            if (!File.Exists(_tsvFilePath))
+            if (File.Exists(CsvFilePath) == false)
             {
-                Debug.LogError("TSV file not found: " + _tsvFilePath);
+                Debug.LogError("CSV file not found: " + CsvFilePath);
                 return;
             }
 
@@ -61,7 +80,7 @@ namespace qbot.Utility
 
             try
             {
-                using (var reader = new StreamReader(_tsvFilePath, Encoding.UTF8))
+                using (var reader = new StreamReader(CsvFilePath, Encoding.UTF8))
                 {
                     // Skip the first line as it is the header
                     reader.ReadLine();
@@ -74,14 +93,14 @@ namespace qbot.Utility
                         var values = line.Split('\t');
                         if (values.Length < 3) continue; // Expecting columns: ID, Korean, English
 
-                        if (int.TryParse(values[0].Trim(), out var key))
-                        {
-                            var koreanValue = values[1].Trim('"').Trim(); // Korean text
-                            var englishValue = values[2].Trim('"').Trim(); // English text
+                        if (int.TryParse(values[0].Trim(), out var key) == false)
+                            continue;
 
-                            koreanDataList.Add(new TextData { Key = key, Value = koreanValue });
-                            englishDataList.Add(new TextData { Key = key, Value = englishValue });
-                        }
+                        var koreanValue = values[1].Trim('"').Trim(); // Korean text
+                        var englishValue = values[2].Trim('"').Trim(); // English text
+
+                        koreanDataList.Add(new TextData { Key = key, Value = koreanValue });
+                        englishDataList.Add(new TextData { Key = key, Value = englishValue });
                     }
                 }
 
@@ -93,11 +112,11 @@ namespace qbot.Utility
                 EditorUtility.SetDirty(_koreanDataContainer);
                 EditorUtility.SetDirty(_englishDataContainer);
 
-                Debug.Log("TSV data import completed successfully.");
+                Debug.Log("CSV data import completed successfully.");
             }
             catch (System.Exception e)
             {
-                Debug.LogError("Error reading TSV file: " + e.Message);
+                Debug.LogError("Error reading CSV file: " + e.Message);
             }
         }
     }
